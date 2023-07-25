891a93c618b490c27285061a613eff3cff98fa1a
Melanikot2 <139865236+Melanikot2@users.noreply.github.com>
2023-07-25 08:06:10 +0000

    lab 5

---
+++ lab_5_dop_task1.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+a = np.ones(10 ** 5)
+b = np.zeros(10 ** 5)
--- lab_5_dop_task2.py
+++ lab_5_dop_task2.py
@@ -1,4 +1,16 @@
 import random
 
 def rand(n, a):
-    b = random.randint()
+    b = random.randint(0, n - 1)
+    i = 0
+
+    while i == 0:
+        i = 1
+        for j in range(0, len(a), 1):
+            if a[j] == b:
+                i = 0
+                b = random.randint(0, n - 1)
+
+    return b
+
+print('Число:', rand(n, a))
