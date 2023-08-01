2c5d3ca592d1db11c3c6e9c3861c74b07e29d495
Melanikot2 <139865236+Melanikot2@users.noreply.github.com>
2023-08-01 08:55:39 +0000

    less 6

--- lab_6_osn_task3.py
+++ lab_6_osn_task3.py
@@ -1,3 +1,16 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-def ellips(x_min, x_max, N)
+def ellips(N, a, b):
+    x = np.linspace(-a, a, N)
+    y = np.linspace(-b, b, N)
+
+    X, Y = np.meshgrid(x, y)
+
+    f_xy = X ** 2 / a ** 2 + Y ** 2 / b ** 2 - 1
+
+    plt.contour(X, Y, f_xy)
+    plt.axis('equal')
+    plt.savefig('lab_6_osn_task3.png')
+
+ellips(N=40, a=10, b=3)
--- lec_6_neyavnoe.py
+++ lec_6_neyavnoe.py
@@ -5,12 +5,12 @@ def circle_plotter(radius=10):
     x = np.arange(-2*radius, 2*radius, 0.1)
     y = np.arange(-2*radius, 2*radius, 0.1)
 
-    X, Y = np.meshgid(x, y)
+    X, Y = np.meshgrid(x, y)
 
     fxy = X ** 2 + Y ** 2 - radius ** 2
 
     plt.contour(X, Y, fxy, levels=[radius**2])
     plt.axis('equal')
-    plt.savefig()
+    plt.savefig('pic_4.png')
 
-circle_plotter()
+circle_plotter(radius=10)
