4a6db47fab544ffc1b63e1b3f63fb0309953ab52
Melanikot2 <139865236+Melanikot2@users.noreply.github.com>
2023-08-06 18:24:06 +0000

    lab 6

---
+++ lab_6_dop_task1.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+def lissazhu(B, t, N, b, a=1, A=1, delta=np.pi/2):
+    t = np.linspace(0, t, N)
+    X = []
+    Y = []
+    for i in t:
+        x = A * np.sin(a * i + delta)
+        y = B * np.sin(b * i)
+        X.append(x)
+        Y.append(y)
+    plt.plot(X, Y)
+    plt.axis('equal')
+    plt.savefig('lissazhu.png')
+
+lissazhu(B=12, t=50, N=500, b=0.6)
--- lab_6_osn_task4.py
+++ lab_6_osn_task4.py
@@ -5,17 +5,16 @@ def arh_spiral(k, N):
     f = np.linspace(0, 8 * np.pi, N)
     x = []
     y = []
-    f_xy = []
     for i in range(0, N):
         x.append(k * f[i] * np.cos(f[i]))
         y.append(k * f[i] * np.sin(f[i]))
+
         X, Y = np.meshgrid(x, y)
 
-        f_xy.append(X ** 2 + Y ** 2 - (k * f[i]) ** 2)
+    f_xy = X ** 2 + Y ** 2 - (k * f) ** 2
 
-    plt.contour(X, Y, f_xy)
+    plt.contour(X, Y, f_xy, levels=0)
     plt.axis('equal')
     plt.savefig('arh_spiral.png')
 
-arh_spiral(k=1, N=500)
-
+arh_spiral(k=1, N=1000)
