36f07c51a9b583d2a2e8b6b14cb3fa98db76289a
Melanikot2 <139865236+Melanikot2@users.noreply.github.com>
2023-08-06 19:25:36 +0000

    lab 7

---
+++ lab_7_osn_task1.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def cikloida(R, t_min, t_max):
+    t = np.linspace(t_min, t_max, 500)
+
+    x = R * (t - np.sin(t)**3)
+    y = R * (1 - np.cos(t)**3)
+
+    plt.plot(x, y)
+    plt.axis('equal')
+    plt.savefig('cikloida.png')
+
+cikloida(R=5, t_min=0, t_max=40)
+
+
+def astroida(R, t_min, t_max):
+    t = np.linspace(t_min, t_max, 500)
+
+    x = R * np.cos(t)**3
+    y = R * np.sin(t)**3
+
+    plt.plot(x, y)
+    plt.axis('equal')
+    plt.savefig('astroida.png')
+
+astroida(R=8, t_min=0, t_max=4)
