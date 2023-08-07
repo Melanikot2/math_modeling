0ec790054a3302c22e22b61cdfdec2002b768681
Melanikot2 <139865236+Melanikot2@users.noreply.github.com>
2023-08-07 18:47:25 +0000

    lab 7

--- lab_7_osn_task2.py
+++ lab_7_osn_task2.py
@@ -4,21 +4,20 @@ from matplotlib.animation import FuncAnimation
 
 fig, ax = plt.subplots()
 
-anim_object = plt.plot([], [], 'o', lw=2)
+anim_object = plt.plot([], [], 'o')
 
 xdata, ydata = [0], [0]
 
 ax.set_xlim(-100, 100)
 ax.set_ylim(-100, 100)
 
-def circle(a, f):
-    for i in np.arange(0, 50, 1):
-        xdata.append(a * i * np.cos(f))
-        ydata.append(a * i * np.sin(f))
-        anim_object.set_data(xdata, ydata)
+def circle(n, frame):
+    xdata.append(n * frame * np.cos(f))
+    ydata.append(n * frame * np.sin(f))
+    anim_object.set_data(xdata, ydata)
     return anim_object
 
-ani = FuncAnimation(fig, circle, a=2, f=np.arange(0, 2 * np.pi, 0.1), interval=500)
+ani = FuncAnimation(fig, circle, n=2, frames=np.arange(0, 50, 1), f=np.arange(0, 2 * np.pi, 0.1), interval=500)
 
 
 ani.save('circle.gif')
