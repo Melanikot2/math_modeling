adb7879ca2de58aeb366ca43ec08811f15f79965
Melanikot2 <139865236+Melanikot2@users.noreply.github.com>
2023-08-07 18:01:05 +0000

    lab 6

--- lab_6_dop_task4.py
+++ lab_6_dop_task4.py
@@ -1,8 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def ladder(N, ):
-    x = np.arange(-100, 100)
-    N = np.arange(-100, 100)
-    for i in N:
-        if x
+def ladder(n):
+    x = []
+    y = [0]
+    
+    for i in range(n+1):
+        x.append(i)
+        x.append(i)
+    
+    for j in range(1, n+1, 1):
+        y.append(j)
+        y.append(j)
+    y.append(n)
+
+    plt.plot(x, y, color='m')
+    plt.title(f'Лесенка из {n} ступенек')
+    plt.axis('equal')
+    plt.savefig('ladder.png')
+
+ladder(n=7)
