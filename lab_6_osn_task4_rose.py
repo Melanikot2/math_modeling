cf3fd51f0f8fa4ab08364c1475615a109cbbf644
Melanikot2 <139865236+Melanikot2@users.noreply.github.com>
2023-08-11 14:45:29 +0000

    lab 6

---
+++ lab_6_osn_task4_rose.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+
+phi = np.linspace(0, 20*np.pi, 1000000)
+
+k = 50
+r = np.sin(k * phi)
+
+x = r * np.cos(phi)
+y = r * np.sin(phi)
+
+plt.plot(x, y)
+plt.title('Rose')
+plt.axis('equal')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.savefig('rose.png')
