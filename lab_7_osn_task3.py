5fc8bf55eda668e96ac109d83afa94e014545274
Melanikot2 <139865236+Melanikot2@users.noreply.github.com>
2023-08-11 15:18:55 +0000

    lab 7

--- lab_7_osn_task3.py
+++ lab_7_osn_task3.py
@@ -4,24 +4,23 @@ import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
 
-ball, = plt.plot([], [], 'o')
+butterfly = plt.plot([], [])
 
 xdata, ydata = [0], [0]
 
-ax.set_xlim(-100, 100)
-ax.set_ylim(-100, 100)
+ax.set_xlim(-5, 5)
+ax.set_ylim(-5, 5)
 
-def circle(t, alpha):
-    phi = np.linspace(0, 2*np.pi, 1000)
-    x = alpha * t * np.cos(phi)
+def butterfly(t=np.linspace(0, 12*np.pi, 10000)):
+    x = np.sin(t) * (np.exp(np.cos(t)) - 2)
     y = alpha * t * np.sin(phi)
     return x, y
 
 plt.axis('equal')
 
 def animate(i):
-    ball.set_data(circle(t=i, alpha=2))
+    ball.set_data(butterfly(t=i, alpha=2))
 
 ani = animation.FuncAnimation(fig, animate, frames = 500, interval=1000)
 
-ani.save('circle.gif')
+ani.save('butterfly.gif')
