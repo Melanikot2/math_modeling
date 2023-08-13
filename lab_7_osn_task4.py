8f180965ea3445598aaae5146372c521134fae46
Melanikot2 <139865236+Melanikot2@users.noreply.github.com>
2023-08-13 19:31:00 +0000

    lab 7

--- lab_7_osn_task4.py
+++ lab_7_osn_task4.py
@@ -8,20 +8,21 @@ dich = plt.plot([], [], '-')
 
 xdata, ydata = [0.1], [0.1]
 
-ax.set_xlim(0, 1000)
-ax.set_ylim(0, 1000)
+ax.set_xlim(-100, 100)
+ax.set_ylim(-100, 100)
 
-def anim(n):
-    C=0.3
-    D=0.33
-    for i in range(1, n + 1, 1):
-        xdata.append(xdata[i-1] ** 2 - ydata[i-1] ** 2 + C)
-        ydata.append(2 * xdata[i-1] * ydata[i-1] + D)
+def anim(n=np.arange(0, 20, 1)):
+    C = 0.3
+    D = 0.33
+    a = xdata[n]
+    b = ydata[n]
+    xdata.append(a ** 2 - b ** 2 + C)
+    ydata.append(2 * a * b + D)
     dich.set_data(xdata, ydata)
     return dich,
 
 plt.axis('equal')
 
-ani = animation.FuncAnimation(fig, anim, frames=800, n=10, interval=100)
+ani = animation.FuncAnimation(fig, anim, frames=1000, interval=30)
 
 ani.save('dich.gif')
