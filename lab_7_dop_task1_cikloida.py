bb93b525eea09b8fca9f08e43d1893bcd76acef1
Melanikot2 <139865236+Melanikot2@users.noreply.github.com>
2023-08-14 07:21:42 +0000

    lab 7

--- lab_7_dop_task1_cikloida.py
+++ lab_7_dop_task1_cikloida.py
@@ -4,23 +4,22 @@ import numpy as np
 
 
 fig, ax = plt.subplots()
-ball, = plt.plot([], [], 'o')
+ball, = plt.plot([], [], '-')
 
 def cikloida_move(R, time):
     t = time
-
     x = R * (t - np.sin(t))
     y = R * (1 - np.cos(t))
     return x, y
 
 edge = 10
 plt.axis('equal')
-ax.set_xlim(0, edge)
+ax.set_xlim(-edge, edge)
 ax.set_ylim(-edge, edge)
 
 def animate(i):
     ball.set_data(cikloida_move(R=5, time=i))
 
-ani = animation.FuncAnimation(fig, animate, frames=1000, interval=100)
+ani = animation.FuncAnimation(fig, animate, frames=200, interval=100)
 
 ani.save('cikloida_move.png')
